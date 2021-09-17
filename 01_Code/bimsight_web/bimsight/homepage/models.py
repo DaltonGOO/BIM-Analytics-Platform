5d898894ec3a9100ea0ddcc7c65d80dccabab419
The BIM Coordinator <30426235+DaltonGOO@users.noreply.github.com>
2021-09-17 14:39:37 -0500

    testing apps

--- 01_Code/bimsight_web/bimsight/homepage/models.py
+++ 01_Code/bimsight_web/bimsight/homepage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class loginInfo(models.Model):
+    SEX_CHOICES = [{'M', 'Male'}, {'F', 'Female'}]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=6, choices=SEX_CHOICES, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField('vaccine', blank=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
